b4007f54e99676f6e94c62c03b5c318c473cd28d
Joseperez <51133263+joseperu2503@users.noreply.github.com>
2023-10-05 00:51:01 -0500

    preload all modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
 import { authGuard } from './core/guards/auth/auth.guard';
 import { verifyAuthGuard } from './core/guards/verify-auth/verify-auth.guard';
@@ -33,7 +33,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
