1f2b805a48c53fdc8379a1ed2040717ffc8f60a5
joseperu2503 <joseperu2503@gmail.com>
2024-03-02 17:37:02 -0500

    HttpClientTestingModule

--- src/app/modules/home/services/home/home.service.spec.ts
+++ src/app/modules/home/services/home/home.service.spec.ts
@@ -1,16 +1,55 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HomeService } from './home.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { FilterData } from '../../models/filter-data.model';
+import { environment } from 'src/environments/environment';
 
-describe('HomeService', () => {
+fdescribe('HomeService', () => {
   let service: HomeService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(HomeService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('tests for getFilterData', () => {
+    it('should return a filter data', (doneFn) => {
+      //Arrange
+      const mockData: FilterData = {
+        brands: [],
+        categories: [],
+        genders: [],
+        sizes: [],
+      };
+
+      //Act
+      service.getFilterData().subscribe((response) => {
+        //Asert
+        expect(response.brands.length).toEqual(mockData.brands.length);
+        expect(response.categories.length).toEqual(mockData.categories.length);
+        expect(response.genders.length).toEqual(mockData.genders.length);
+        expect(response.sizes.length).toEqual(mockData.sizes.length);
+
+        doneFn();
+      });
+
+      //http config
+      const url = `${environment.APP_API_URL}/products/filter-data`;
+      const req = httpController.expectOne(url);
+      req.flush(mockData);
+      httpController.verify();
+    });
   });
 });
